7807858d9abdcd182f2d99cb54e9109c4e99974c
spaenleh <spaenleh@gmail.com>
2024-03-26 17:18:11 +0100

    chore: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'docker',
+      password: 'docker',
+      database: 'docker',
+      entities: [],
+      // the backend is supposed to synchronize the entities
+      synchronize: false,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
